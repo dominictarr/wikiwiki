5d0942552ef982eb7a175873eab33a7624c09f02
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-19 16:51:20 -0500

    browserify will set process.title="browser" - more robust to check that than process.title=="node"

--- udid.js
+++ udid.js
@@ -1,4 +1,4 @@
-module.exports = 'node' == process.title 
+module.exports = 'browser' != process.title 
   ? require('udid')('wikiwiki')  //generate an ID.
   : require('count-tabs')().id   //generate an ID for this tab.
 
